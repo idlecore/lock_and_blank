308176a0fc59bbf5ca1b10d0c7d966eabbf4c896
Carter Hay <clccmh@gmail.com>
2024-06-14 17:51:41 -0500

    Improve build script

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
-cl.exe lock_and_blank.cpp
+@echo off
+if not exist "bin" mkdir "bin"
+if not exist "obj" mkdir "obj"
+@echo on
+
+cl.exe /O2 /W4 /Fe:.\\bin\\lock_and_blank.exe /Fo"obj\\" src\lock_and_blank.cpp
